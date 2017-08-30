758f6cf6b8af2af76f779e3e681f30d78a792091
rhclayto <h@obscur.us>
2017-08-30 17:03:59 -0600

    Update angular-electron.js

--- angular-electron.js
+++ angular-electron.js
@@ -106,4 +106,4 @@ angular.module('angular-electron').service('safeShutdown', ['$q', 'currentWindow
   };
 }]);
 
-})(window.angular, window.nodeRequire, window.process);
+})(window.angular, window.require || window.nodeRequire, window.process);
